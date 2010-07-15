46206f43c03c3c0aece3d079b5818b5ca271b8f3
Alexey Vyskubov <alexey@mawhrin.net>
2010-07-15 18:23:11 +0300

    First version of move generator (without checking validity)

--- whitecamel.ml
+++ whitecamel.ml
@@ -86,6 +86,108 @@ let possible_moves = function
   | Sente, _ -> mv_sente_gold
   | Gote, _ -> mv_gote_gold
 
+let check_no_slide (board : cell array array) side piece i j (dx, dy) =
+  (* May throw 'Invalid_argument' if move is out of the board's borders *)
+  let ni = i + dx in
+  let nj = j + dy in
+  let t = board.(ni).(nj) in
+  match t with
+    | Some (x, _) when x = side -> [] (* cannot eat own piece *)
+    | _ ->
+      let st = Some (i, j) in
+      let fn = (ni, nj) in
+      if (side = Sente && nj < 4 && j < 4) || (side = Gote && nj > 0 && j > 0)
+      then (* the move is not to or from promotion area *)
+	[{what = piece; start = st; finish = fn}]
+      else (* promotion is possible *)
+	begin
+	  match snd piece with
+	    | Pawn ->
+	      [{what = (side, Tokin); start = st; finish = fn}]
+	    | Silver -> (* It may make sense not to promote silver general *)
+	      [{what = (side, Silver); start = st; finish = fn};
+	       {what = (side, GoldS); start = st; finish = fn}]
+	    | Bishop ->
+	      [{what = (side, DragonKing); start = st; finish = fn}]
+	    | Rook ->
+	      [{what = (side, DragonHorse); start = st; finish = fn}]
+	    | _ -> (* nothing else can be promoted *)
+	      [{what = piece; start = st; finish = fn}]
+	end (* possible promotion *)
+	  
+let rec check_slide_r found board side piece i j (dx, dy) =
+  (* the 'start' value in returned moves is wrong *)
+  try
+    let one = check_no_slide board side piece i j (dx, dy) in
+    let ni = i + dx in
+    let nj = j + dy in
+    let res = one::found in
+    if board.(ni).(nj) = None
+    then check_slide_r res board side piece (i+dx) (j+dy) (dx, dy)
+    else res
+  with Invalid_argument _ -> found
+
+let check_slide board side piece i j delta =
+  let fix_move m =
+    match m.start with
+      | Some (x, y) when (x = i && y = j) -> m
+      | _ -> {what = piece; start = Some (i, j); finish = m.finish } in
+  let sliding_moves = check_slide_r [] board side piece i j delta in
+  List.map fix_move (List.flatten sliding_moves)
+
+let check_one (board : cell array array) side piece i j mv =
+  match mv with
+    | (JustOne, (dx, dy)) ->
+      begin
+	try check_no_slide board side piece i j (dx, dy)
+	with Invalid_argument _ -> [] (* the move is out of the board's borders *)
+      end
+    | (Slide, (dx, dy)) -> check_slide board side piece i j (dx, dy)
+
+let rec check_opm_r found (board : cell array array) side piece i j pm =
+    match pm with
+      | [] -> found
+      | hd :: tl ->
+	check_opm_r ((check_one board side piece i j hd) @ found) board side piece i j tl
+
+let check_opm (board : cell array array) side piece i j pm =
+  check_opm_r [] board side piece i j pm 
+;;
+let gopm (board : cell array array) side piece i j =
+  let pm = possible_moves piece in
+  check_opm board side piece i j pm
+;;
+let generate_drops hand side i j =
+  let generate_one pc =
+    { what = (side, pc); start = None; finish = (i, j)} in
+  List.map generate_one hand
+;;
+let rec find_all_moves_r found brd (i, j) hand side = 
+  if j == 5
+  then found
+  else let incr (i, j) =
+	 begin
+	   match (i, j) with
+	     | (4, _) -> (0, j+1)
+	     | _ -> (i+1, j)
+	 end
+       in
+       match brd.(i).(j) with
+ 	 | None ->
+	   let drops = generate_drops hand side i j in
+	   find_all_moves_r (drops @ found) brd (incr (i, j)) hand side
+	 | Some (s, p) when s = side ->
+	   let mvs = gopm brd side (s, p) i j in
+	   find_all_moves_r (mvs @ found) brd (incr (i, j)) hand side
+	 (* and if not our color... *)
+	 | _ -> find_all_moves_r found brd (incr (i, j)) hand side
+
+let find_all_moves pos side =
+  let hand = if side == Sente then pos.sente_hand else pos.gote_hand in
+  let board = pos.board in
+  find_all_moves_r [] board (0, 0) hand side
+  
+
 ;; (* ---------------------------------------- *)
 
 let print_position pos =
