8e17d3d714ea8e815b09471b2f23c6243b88b740
Alexey Vyskubov <alexey@mawhrin.net>
2010-07-16 14:02:05 +0300

    Cleanup.

--- moves.ml
+++ moves.ml
@@ -8,7 +8,7 @@ type move = {
   finish : int * int ;
 }
 
-type _sliding = Slide | JustOne
+type _sliding = Slide | Step
 
 (* "Forward" for sente side *)
 let m_sente = [(0, 1)]
@@ -29,19 +29,19 @@ let m_raw = m_sente @ m_gote @ m_sides
    the element and the list *)
 
 (* Pawn moves one step forward *)
-let mv_sente_pawn = JustOne @* m_sente
-let mv_gote_pawn = JustOne @* m_gote
+let mv_sente_pawn = Step @* m_sente
+let mv_gote_pawn = Step @* m_gote
 
 (* King moves one step vertically or horizontaly or diagonaly *)
-let mv_king = JustOne @* (m_diag @ m_raw)
+let mv_king = Step @* (m_diag @ m_raw)
 
 (* Gold general moves vertically or horizontally or forward diagonally *)
-let mv_sente_gold = JustOne @* (m_raw @ m_sente_diag)
-let mv_gote_gold =  JustOne @* (m_raw @ m_gote_diag)
+let mv_sente_gold = Step @* (m_raw @ m_sente_diag)
+let mv_gote_gold =  Step @* (m_raw @ m_gote_diag)
 
 (* Silver general moves diagonally and forward *)
-let mv_sente_silver = JustOne @* (m_diag @ m_sente)
-let mv_gote_silver = JustOne @* (m_diag @ m_gote)
+let mv_sente_silver = Step @* (m_diag @ m_sente)
+let mv_gote_silver = Step @* (m_diag @ m_gote)
 
 (* Bishop moves diagnally, sliding *)
 let mv_bishop = Slide @* m_diag
@@ -51,10 +51,10 @@ let mv_rook = Slide @* m_raw
 
 (* Dragon horse (promoted bishop) moves like bishop or one step
    horizontally or vertically *)
-let mv_dragonhorse = mv_bishop @ (JustOne @* m_raw)
+let mv_dragonhorse = mv_bishop @ (Step @* m_raw)
 
 (* Dragon king (promoted rook) moves like rook or one step diagonally *)
-let mv_dragonking = mv_rook @ (JustOne @* m_diag)
+let mv_dragonking = mv_rook @ (Step @* m_diag)
 
 let possible_moves = function
   | Sente, Pawn -> mv_sente_pawn
@@ -72,12 +72,14 @@ let possible_moves = function
   | Gote, _ -> mv_gote_gold
 
 let check_step (brd, side, piece, (i, j)) (dx, dy) =
+
   (* Returns the list of the moves the given piece may do
    * from (i, j) point by moving one step along (dx, dy) vector.
    * Raises 'Invalid argument' when the move is out of the board's borders.
    * Raises 'Not found' when the move is blocked by own piece.
    * Otherwise returns the list of one or two (when promotion is available)
    * moves. *)
+
   let ni, nj = i + dx, j + dy in
   let t = brd.(ni).(nj) in (* may raise Invalid_argument here *)
   match t with
@@ -85,7 +87,11 @@ let check_step (brd, side, piece, (i, j)) (dx, dy) =
     | _ ->
       let st = Some (i, j) in
       let fn = (ni, nj) in
+
+      (* The move without promotion *)
       {what = piece; start = st; finish = fn} ::
+
+          (* Adding possible promotion move *)
 	  if side = Sente && nj < 4 && j < 4 || side = Gote && nj > 0 && j > 0
 	  then [] (* the move is not to or from promotion area *)
 	  else match snd piece with (* promotion is possible *)
@@ -96,6 +102,7 @@ let check_step (brd, side, piece, (i, j)) (dx, dy) =
             | _ -> [] (* nothing else can be promoted *)
           
 let rec check_slide_r acc (brd, side, piece, (i, j)) (dx, dy) =
+
   (* Constructs the list of all sliding moves of the given piece from (i, j)
    * along (dx, dy) vector.  NB: the 'start' value in returned moves
    * may be wrong since it may not be the real start value for the move.
@@ -115,7 +122,13 @@ let rec check_slide_r acc (brd, side, piece, (i, j)) (dx, dy) =
 
 
 let check_slide situation delta =
+
+  (* Returns the list of all sliding moves in the given situation
+   * (meaning given piece on the board) along delta vector *)
+
   let (_, _, piece, (i, j)) = situation in
+  (* 'start' value in themoves, returned by check_slide_r, may be wrong,
+   * so it should be fixed here *)
   let fix_move m =
     match m.start with
       | Some (x, y) when x = i && y = j -> m
@@ -123,14 +136,21 @@ let check_slide situation delta =
   let sliding_moves = check_slide_r [] situation delta in
   List.map fix_move (List.flatten sliding_moves)
 
+
 let check_one_dir situation mv =
+
+  (* Returns the list of moves for the given situation (piece on board)
+   * according to the move rule mv *)
+
   match mv with
-    | (JustOne, delta) ->
+    | (Step, delta) ->
       begin
         try check_step situation delta
         with
-	  | Invalid_argument _ -> [] (* the move is out of the board's borders *)
-	  | Not_found _ -> [] (* the move is blocked by own piece *)
+	  | Invalid_argument _ ->
+	    [] (* the move is out of the board's borders *)
+	  | Not_found _ ->
+	    [] (* the move is blocked by own piece *)
       end
     | (Slide, delta) -> check_slide situation delta
 
