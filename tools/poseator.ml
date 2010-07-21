56c4154accab4848c6d7e8168fbfbbfb86426eda
Alexey Vyskubov <alexey@mawhrin.net>
2010-07-21 11:51:56 +0300

    Board design changed, leaving place for cursor. Cursor drawing code done.

--- tools/poseator.ml
+++ tools/poseator.ml
@@ -9,12 +9,14 @@ type cursor_t = {
   mutable y : int
 }
 
-let left_corner_x = 5
-let left_corner_y = 5
-
 let cursor = { x = 0; y = 0 }
 
 let win = initscr ()
+let symbols = get_acs_codes ()
+
+let space = int_of_char ' '
+let more = int_of_char '>'
+let less = int_of_char '<'
 
 let do_init () =
   let _ = noecho () in
@@ -27,45 +29,34 @@ let do_init () =
 
 let normal_colors () = attrset (A.color_pair 0)
 let red_pieces () = attrset (A.color_pair 1 lor A.bold)
-
-let symbols = get_acs_codes ()
+let bold () = attrset (A.color_pair 0 lor A.bold)
 
 let cur_pos = ref start_position
 
-let hline2 y x  =
-  let _ = mvaddch y x symbols.hline in
-  let _ = addch symbols.hline in
-  ()
-
 let board_skeleton y x =
   let _ = mvaddch y x symbols.ulcorner in
-  let _ = mvaddch y (x + 15) symbols.urcorner in
+  let _ = mvaddch y (x + 25) symbols.urcorner in
   let _ = mvaddch  (y + 10) x symbols.llcorner in
-  let _ = mvaddch (y + 10) (x + 15) symbols.lrcorner in
+  let _ = mvaddch (y + 10) (x + 25) symbols.lrcorner in
   let _ =
     for i = 0 to 5 do
       for j = 0 to 4 do
-	hline2 (y + 2 * i) (x + 3 * j + 1)
-      done
-    done in
-  let _ =
-    for i = 0 to 4 do
-      for j = 0 to 5 do
-	let _ = mvaddch (y + 2 * i + 1) (x + 3 * j) symbols.vline in
+	mvhline (y + 2 * i) (x + 5 * j + 1) symbols.hline 4 ;
+	let _ = mvaddch (y + 2 * j + 1) (x + 5 * i) symbols.vline in
 	()
       done
     done in
   let _ =
     for i = 1 to 4 do
       let t = y + 2 * i in
+      let s = x + 5 * i in
       let _ = mvaddch t x symbols.ltee in
-      let _ = mvaddch t (x + 15) symbols.rtee in
-      let t' = x + 3 * i in
-      let _ = mvaddch y t' symbols.ttee in
-      let _ = mvaddch (y + 10) t' symbols.btee in
+      let _ = mvaddch t (x + 25) symbols.rtee in
+      let _ = mvaddch y s symbols.ttee in
+      let _ = mvaddch (y + 10) s symbols.btee in
       let _ =
 	for j = 1 to 4 do
-	  let _ = mvaddch t (3 * j + x) symbols.plus in
+	  let _ = mvaddch t (5 * j + x) symbols.plus in
 	  ()
 	done in
       ()
@@ -74,13 +65,12 @@ let board_skeleton y x =
 
 let empty_cell () =
   let _ = normal_colors () in
-  let _ = addch symbols.bullet in
-  let _ = addch symbols.bullet in
+  let _ = hline symbols.bullet 2 in
   ()
-;;
+
 let draw_piece x y pc (* board coordinates, not cursed coordinates *) =
-  let cursed_y = left_corner_y + 9 - 2 * y in
-  let cursed_x = left_corner_x + 1 + 3 * x in
+  let cursed_y = 9 - 2 * y in
+  let cursed_x = 2 + 5 * x in
   let _ = move cursed_y cursed_x in
   match pc with
     | None -> empty_cell ()
@@ -107,12 +97,26 @@ let draw_piece x y pc (* board coordinates, not cursed coordinates *) =
 	end in
       ()
 
-;;
+let update_cursor x y =
+  let x' = cursor.x in
+  let y' = cursor.y in
+  let _ = cursor.x <- x in
+  let _ = cursor.y <- y in
+  let cy n = 9 - 2 * n in
+  let cx1 n = 5 * n + 1 in
+  let cx2 n = 5 * n + 4 in
+  let _ = mvaddch (cy y') (cx1 x') space in
+  let _ = mvaddch (cy y') (cx2 x') space in
+  let _ = mvaddch (cy y) (cx1 x) more in
+  let _ = mvaddch (cy y) (cx2 x) less in
+  ()
 
 let _ = 
   let () = do_init () in
   let _ = normal_colors () in
-  let _ = board_skeleton left_corner_y left_corner_x  in
+  let () = board_skeleton 0 0 in
+  let _ = bold () in
+  let () = update_cursor 0 0 in
   let brd = !cur_pos.Types.board  in
   let _ = 
     for i = 0 to 4 do
