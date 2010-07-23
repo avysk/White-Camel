96e9471d9596b75317e4ed91abda75d9ba182230
Alexey Vyskubov <alexey@mawhrin.net>
2010-07-23 13:19:28 +0300

    a couple of TODOs in comments

--- check.mli
+++ check.mli
@@ -1,3 +1,5 @@
 open Types
 
 val under_check : position -> side -> bool
+(* TODO to find all checks with their descriptions, to feed in a move finding
+ * function *)
--- moves.mli
+++ moves.mli
@@ -2,6 +2,8 @@ open Types
 open Position
 open Rules
 
+(* TODO separate function to find moves in the case of check *)
+
 (* Generate all moves in the given position for the given side to move.  The
  * validity of moves (check situation, pawn drops) is not checked.  Includes but
  * does not force promotions. *)
