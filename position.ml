26ce862f5a7f90132f76abaca7d33af3fa452973
Alexey Vyskubov <alexey@mawhrin.net>
2010-07-26 23:35:56 +0300

    Cleaning up: removing opens and quilifying the stuff

--- position.ml
+++ position.ml
@@ -1,14 +1,12 @@
 open Utils
-open Types
-open Rules
 
 (* Checks if the king is attacked via one-step move by some side' piece *)
 let under_check_close brd side' king =
   let fw =
     match side' with
     (* Meaning of "forward" for the king *)
-    | Sente -> -1
-    | Gote -> 1
+    | Types.Sente -> -1
+    | Types.Gote -> 1
   in
   let piece_at delta pcs =
     try
@@ -20,18 +18,18 @@ let under_check_close brd side' king =
     with Invalid_argument _ -> false
   in
   (* forward from king *)
-  piece_at (0, fw) forward_attackers ||
+  piece_at (0, fw) Rules.forward_attackers ||
   (* forward diagonals from king *)
-  piece_at (-1, fw) forward_diag_attackers ||
-  piece_at (1, fw) forward_diag_attackers ||
+  piece_at (-1, fw) Rules.forward_diag_attackers ||
+  piece_at (1, fw) Rules.forward_diag_attackers ||
   (* sideways from king *)
-  piece_at (-1, 0) sideways_attackers ||
-  piece_at (1, 0) sideways_attackers ||
+  piece_at (-1, 0) Rules.sideways_attackers ||
+  piece_at (1, 0) Rules.sideways_attackers ||
   (* backward from king *)
-  piece_at (0, -fw) backward_attackers ||
+  piece_at (0, -fw) Rules.backward_attackers ||
   (* backward diagonals from king *)
-  piece_at (-1, -fw) backward_diag_attackers ||
-  piece_at (1, -fw) backward_diag_attackers
+  piece_at (-1, -fw) Rules.backward_diag_attackers ||
+  piece_at (1, -fw) Rules.backward_diag_attackers
 
 let under_check_far brd side' king =
   let rec piece_along current delta pcs =
@@ -41,7 +39,8 @@ let under_check_far brd side' king =
         match brd @@ next with
         (* If cell is empty, continue search *)
         | None -> piece_along next delta pcs
-        (* If there's a piece in a cell, check if is the one we're searching for *)
+        (* If there's a piece in a cell, check if is the one
+         * we're searching for *)
         | Some (s, p) when s = side' -> List.mem p pcs
         (* Piece belonging to the other side block checks *)
         | _ -> false
@@ -49,23 +48,23 @@ let under_check_far brd side' king =
     (* If board is over, no attack from this line *)
     with Invalid_argument _ -> false
   in
-  piece_along king (0, 1) straight_sliders ||
-  piece_along king (0, -1) straight_sliders ||
-  piece_along king (1, 0) straight_sliders ||
-  piece_along king (-1, 0) straight_sliders ||
-  piece_along king (1, 1) diag_sliders ||
-  piece_along king (-1, 1) diag_sliders ||
-  piece_along king (1, -1) diag_sliders ||
-  piece_along king (-1, -1) diag_sliders
+  piece_along king (0, 1) Rules.straight_sliders ||
+  piece_along king (0, -1) Rules.straight_sliders ||
+  piece_along king (1, 0) Rules.straight_sliders ||
+  piece_along king (-1, 0) Rules.straight_sliders ||
+  piece_along king (1, 1) Rules.diag_sliders ||
+  piece_along king (-1, 1) Rules.diag_sliders ||
+  piece_along king (1, -1) Rules.diag_sliders ||
+  piece_along king (-1, -1) Rules.diag_sliders
 
 let under_check position side =
   let king =
     match side with
-    | Sente -> position.sente_king
-    | Gote -> position.gote_king
+    | Types.Sente -> position.Types.sente_king
+    | Types.Gote -> position.Types.gote_king
   in
-  let side' = other side in
-  let brd = position.board in
+  let side' = Types.other side in
+  let brd = position.Types.board in
   under_check_close brd side' king || under_check_far brd side' king
 
 let init_position plist stm shd ghd =
@@ -73,7 +72,7 @@ let init_position plist stm shd ghd =
   let put_piece (x, y, p) = ar.(x).(y) <- Some p in
   let () = List.iter put_piece plist in
   {
-    board = ar ;
+    Types.board = ar ;
     to_move = stm ;
     sente_hand = shd ;
     gote_hand = ghd ;
@@ -83,47 +82,49 @@ let init_position plist stm shd ghd =
 
 let apply_move position move =
   let (pc, st, (fx, fy)) = move in
-  let brd' = copy_board position.board in
-  let mv = position.to_move in
-  let shand = position.sente_hand in
-  let ghand = position.gote_hand in
-  let sking = position.sente_king in
-  let gking = position.gote_king in
+  let brd' = copy_board position.Types.board in
+  let mv = position.Types.to_move in
+  let shand = position.Types.sente_hand in
+  let ghand = position.Types.gote_hand in
+  let sking = position.Types.sente_king in
+  let gking = position.Types.gote_king in
   match st with
   (* drop move *)
   | None ->
       begin
         match mv with
-        | Sente ->
+        | Types.Sente ->
             let shand' = remove_one pc shand in
-            let _ = brd'.(fx).(fy) <- Some (Sente, pc) in
-            {position with board = brd'; to_move = Gote; sente_hand = shand'}
-        | Gote ->
+            let _ = brd'.(fx).(fy) <- Some (Types.Sente, pc) in
+            {position with Types.board = brd';
+             to_move = Types.Gote; sente_hand = shand'}
+        | Types.Gote ->
             let ghand' = remove_one pc ghand in
-            let _ = brd'.(fx).(fy) <- Some (Gote, pc) in
-            {position with board = brd'; to_move = Sente; gote_hand = ghand'}
+            let _ = brd'.(fx).(fy) <- Some (Types.Gote, pc) in
+            {position with Types.board = brd';
+             to_move = Types.Sente; gote_hand = ghand'}
       end
   (* normal move *)
   | Some (sx, sy) ->
       begin
         let sking', gking' =
-          if pc != King then sking, gking
+          if pc != Types.King then sking, gking
           else begin
             match mv with
-            | Sente -> (fx, fy), gking
-            | Gote -> sking, (fx, fy)
+            | Types.Sente -> (fx, fy), gking
+            | Types.Gote -> sking, (fx, fy)
           end in
         let _ = brd'.(sx).(sy) <- None in
         let _ = brd'.(fx).(fy) <- Some (mv, pc) in
         let shand', ghand' =
           begin
-            match position.board.(fx).(fy) with
+            match position.Types.board.(fx).(fy) with
             | None -> shand, ghand
-            | Some (Sente, tpc) -> shand, tpc :: ghand
-            | Some (Gote, tpc) -> tpc :: shand, ghand
+            | Some (Types.Sente, tpc) -> shand, tpc :: ghand
+            | Some (Types.Gote, tpc) -> tpc :: shand, ghand
           end in
-        { board = brd';
-          to_move = other mv;
+        { Types.board = brd';
+          to_move = Types.other mv;
           sente_king = sking';
           gote_king = gking';
           sente_hand = shand';
@@ -131,18 +132,18 @@ let apply_move position move =
       end
 
 let start_position =
-  init_position [(0, 0, (Sente, King));
-                 (1, 0, (Sente, Gold));
-                 (2, 0, (Sente, Silver));
-                 (3, 0, (Sente, Bishop));
-                 (4, 0, (Sente, Rook));
-                 (0, 1, (Sente, Pawn));
-                 (4, 3, (Gote, Pawn));
-                 (0, 4, (Gote, Rook));
-                 (1, 4, (Gote, Bishop));
-                 (2, 4, (Gote, Silver));
-                 (3, 4, (Gote, Gold));
-                 (4, 4, (Gote, King))] Sente [] []
+  init_position [(0, 0, (Types.Sente, Types.King));
+                 (1, 0, (Types.Sente, Types.Gold));
+                 (2, 0, (Types.Sente, Types.Silver));
+                 (3, 0, (Types.Sente, Types.Bishop));
+                 (4, 0, (Types.Sente, Types.Rook));
+                 (0, 1, (Types.Sente, Types.Pawn));
+                 (4, 3, (Types.Gote, Types.Pawn));
+                 (0, 4, (Types.Gote, Types.Rook));
+                 (1, 4, (Types.Gote, Types.Bishop));
+                 (2, 4, (Types.Gote, Types.Silver));
+                 (3, 4, (Types.Gote, Types.Gold));
+                 (4, 4, (Types.Gote, Types.King))] Types.Sente [] []
 
 (*
  vim:sw=2
