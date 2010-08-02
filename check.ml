d4e1f340aecea652e7a4f0b5f53e3016d196e495
Alexey Vyskubov <alexey@mawhrin.net>
2010-08-02 11:02:33 +0300

    Refactoring

--- check.ml
+++ check.ml
@@ -1,14 +1,12 @@
-open Types
 open Utils
-open Rules
 
 let under_check_close brd side' king =
 (* Checks if the king is attacked via one-step move by some side' piece *)
   let fw =
     match side' with
     (* Meaning of "forward" for the king *)
-    | Sente -> -1
-    | Gote -> 1
+    | Types.Sente -> -1
+    | Types.Gote -> 1
   in
   let piece_at delta pcs =
     try
@@ -20,18 +18,18 @@ let under_check_close brd side' king =
     with Invalid_argument _ -> false
   in
   (* forward from king *)
-  piece_at (0, fw) forward_attackers ||
+  piece_at (0, fw) Rules.forward_attackers ||
   (* forward diagonals from king *)
-  piece_at (-1, fw) forward_diag_attackers ||
-  piece_at (1, fw) forward_diag_attackers ||
+  piece_at (-1, fw) Rules.forward_diag_attackers ||
+  piece_at (1, fw) Rules.forward_diag_attackers ||
   (* sideways from king *)
-  piece_at (-1, 0) sideways_attackers ||
-  piece_at (1, 0) sideways_attackers ||
+  piece_at (-1, 0) Rules.sideways_attackers ||
+  piece_at (1, 0) Rules.sideways_attackers ||
   (* backward from king *)
-  piece_at (0, -fw) backward_attackers ||
+  piece_at (0, -fw) Rules.backward_attackers ||
   (* backward diagonals from king *)
-  piece_at (-1, -fw) backward_diag_attackers ||
-  piece_at (1, -fw) backward_diag_attackers
+  piece_at (-1, -fw) Rules.backward_diag_attackers ||
+  piece_at (1, -fw) Rules.backward_diag_attackers
 
 let under_check_far brd side' king =
   let rec piece_along current delta pcs =
@@ -49,22 +47,22 @@ let under_check_far brd side' king =
     (* If board is over, no attack from this line *)
     with Invalid_argument _ -> false
   in
-  piece_along king (0, 1) straight_sliders ||
-  piece_along king (0, -1) straight_sliders ||
-  piece_along king (1, 0) straight_sliders ||
-  piece_along king (-1, 0) straight_sliders ||
-  piece_along king (1, 1) diag_sliders ||
-  piece_along king (-1, 1) diag_sliders ||
-  piece_along king (1, -1) diag_sliders ||
-  piece_along king (-1, -1) diag_sliders
+  piece_along king (0, 1) Rules.straight_sliders ||
+  piece_along king (0, -1) Rules.straight_sliders ||
+  piece_along king (1, 0) Rules.straight_sliders ||
+  piece_along king (-1, 0) Rules.straight_sliders ||
+  piece_along king (1, 1) Rules.diag_sliders ||
+  piece_along king (-1, 1) Rules.diag_sliders ||
+  piece_along king (1, -1) Rules.diag_sliders ||
+  piece_along king (-1, -1) Rules.diag_sliders
 
 let under_check position side =
   let king =
     match side with
-    | Sente -> position.sente_king
-    | Gote -> position.gote_king
+    | Types.Sente -> position.Types.sente_king
+    | Types.Gote -> position.Types.gote_king
   in
-  let side' = other side in
-  let brd = position.board in
+  let side' = Types.other side in
+  let brd = position.Types.board in
   under_check_close brd side' king || under_check_far brd side' king
 
