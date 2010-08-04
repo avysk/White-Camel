ff894660402bd8717b16357dd93ec9834cf8cff6
Alexey Vyskubov <alexey@mawhrin.net>
2010-08-04 13:43:51 +0300

    Global weak hash for gametrees is created (but is not in use yet)

--- gametree.ml
+++ gametree.ml
@@ -1,5 +1,33 @@
 open Utils
+open Types
+
 type gametree = Gametree of (Types.position * (gametree list Lazy.t))
+
+module PosHash = 
+  struct
+    type t = gametree
+    let hash = function
+      | Gametree (pos, _) -> Crc.pos_crc pos
+    let equal gt1 gt2 =
+      let Gametree (pos1, _) = gt1 in
+      let Gametree (pos2, _) = gt2 in
+      pos1.to_move = pos2.to_move &&
+      pos1.sente_king = pos2.gote_king &&
+      pos1.board = pos2.board &&
+      begin
+        let sh1 = pos1.sente_hand in
+        let gh1 = pos1.gote_hand in
+        let sh2 = pos2.sente_hand in
+        let gh2 = pos2.gote_hand in
+        List.length sh1 = List.length sh2 &&
+        List.length gh1 = List.length gh2 &&
+        sh1 @=@ sh2 && gh1 @=@ gh2
+      end
+  end
+
+module WeakPosHash = Weak.Make(PosHash)
+
+let global_weak_pos_hash = WeakPosHash.create 100000 (* TODO: 100000 ? *)
 
 let rec create_gametree pos =
   let apply = lazy (Apply_move.apply_move pos) in
