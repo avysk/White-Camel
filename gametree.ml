39fbd8416e8d931313bdefe647506c11b707e613
Alexey Vyskubov <alexey@mawhrin.net>
2010-08-19 21:42:27 +0300

    speed improvement

--- gametree.ml
+++ gametree.ml
@@ -46,21 +46,16 @@ module WeakPosHash = Weak.Make(PosHash)
 let global_weak_pos_hash = WeakPosHash.create 500000 (* TODO: 500000 ? *)
 
 let rec create_gametree pos =
-  let apply = lazy (Apply_move.apply_move pos) in
-  let apply_moves = lazy (create_gametree $ (Lazy.force apply)) in
-  let all_moves = lazy (Moves.find_all_moves pos pos.to_move) in
+  let apply = Apply_move.apply_move pos in
   let possible = lazy (function
     | Gametree (p, _) -> not (Check.under_check p pos.to_move)) in
-  let all_branches = lazy (
-    List.map
-    (Lazy.force apply_moves)
-    (Lazy.force all_moves)
-  ) in
   let branches = lazy (
     List.filter
-    (Lazy.force possible)
-    (Lazy.force all_branches)
-  ) in
+      (Lazy.force possible)
+      (List.map
+      (create_gametree $ apply)
+      (Moves.find_all_moves pos pos.to_move))
+    ) in
   (* NB: illegal mate by pawn drop should be excluded at position evaluation *)
   let gt_tmp = Gametree (pos, branches) in
   WeakPosHash.merge global_weak_pos_hash gt_tmp
