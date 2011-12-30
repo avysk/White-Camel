eb7b84c79cfc3ae3ef87e497586475eb16f01152
Alexey Vyskubov <tascafoo@gmail.com>
2011-12-30 12:40:12 +0200

    moved git juggling into a separate script

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -2,9 +2,11 @@
 
 open Ocamlbuild_plugin
 
-let get_version = "if [ -z \"$(git diff-index --name-only HEAD)\" ] ; then echo \"let version = \\\"$(git rev-parse HEAD)\\\"\" ; else echo \"let version = \\\"$(git rev-parse HEAD)-modified\\\"\"; fi >"
+(* let get_version = "if [ -z \"$(git diff-index --name-only HEAD)\" ] ; then
+  * echo \"let version = \\\"$(git rev-parse HEAD)\\\"\" ; else echo \"let
+  * version = \\\"$(git rev-parse HEAD)-modified\\\"\"; fi >" *)
 
-let create_version_file _ _ = Cmd (S [ Sh get_version ; P "version.ml" ])
+let create_version_file _ _ = Cmd (S [ A "sh" ; P "../scripts/find-version.sh" ; Sh ">" ; P "version.ml" ])
 
 let () = dispatch begin function
   | After_rules ->
